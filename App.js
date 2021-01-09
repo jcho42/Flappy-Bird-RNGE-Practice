7c4edf58dcc5a206825050f1dd3416911ad04b3f
jcho42 <jason.cho.301230@gmail.com>
2021-01-08 19:34:36 -0600

    feat: create Matter js world and Bird component

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { GameEngine } from 'react-native-game-engine';
+import Matter from 'matter-js';
+
+import Constants from './components/Constants'
+import { Bird } from './components/bodies'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.gameEngine = null;
+    this.entities = this.setupWorld()
+  }
+
+  setupWorld = () => {
+    const engine = Matter.Engine.create();
+    const world = engine.world;
+
+    const bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT / 2, 50, 50)
+
+    Matter.World.add(world, [bird])
+
+    return {
+      physics: {engine, world},
+      bird: {body: bird, color: "red", renderer: Bird}
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-
+        <GameEngine
+          ref={ref => { this.gameEngine = ref }}
+          style={styles.gameContainer}
+          entities={this.entities}
+        />
       </View>
     );
   }
